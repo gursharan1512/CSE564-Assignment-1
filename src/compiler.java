f712fb77b1927a717e34abe245b28a7bf4ade0ac
Manthan Agrawal <manthanagrawal96@gmail.com>
2021-01-26 13:50:56 -0700

    @manthan-pict Updated code to handle multiple condition and code refactoring.

--- src/compiler.java
+++ src/compiler.java
@@ -1,126 +1,186 @@
 import java.io.*;
-import java.util.StringTokenizer;
-import java.util.Vector;
+
+/**
+ * Translate the give block of code to tokens and create a symbol representation for the same.
+ * @author Gursharanjit Singh Ghotra gghotra@asu.edu
+ * @author Manthan Agrawal magraw12@asu.edu
+ */
 
 public class compiler {
     public static void main(String[] args) throws IOException {
 
         if (args.length > 0) {
-            String text = readTextFile(args[0]);
-            //Vector<String[]> lineVector = splitString(text);
-            String[] lineVector = splitString(text);
-            translate(lineVector);
+            String code = readTextFile(args[0]);
+            String[] tokens = getToken(code);
+            translate(tokens);
         }
         else {
-            //raise exception - argument not passed
+            throw new FileNotFoundException("Please pass a file in argument");
         }
-
     }
 
+    /**
+     * Reads File from given path.
+     * @param path input code file path
+     * @return String of code without extra spaces.
+     * @throws IOException file reading exception.
+     */
     public static String readTextFile (String path) throws IOException {
 
-        String text = "";
-        File file = new File(path);
-        BufferedReader br = new BufferedReader(new FileReader( file ));
+        StringBuilder code = new StringBuilder();
+        BufferedReader br = new BufferedReader(new FileReader(path));
         String str;
-        while ( (str = br.readLine()) != null) {
-            text = text + str + " ";
-        }
-        String text2 = text.replaceAll("\\s+", " ");
-        System.out.println(text2);
-        return text2;
+        while ( (str = br.readLine()) != null) code.append(str).append(" ");
+
+        return code.toString().replaceAll("\\s+", " ");
     }
 
-    public static String[] splitString (String text) {
-        return text.split(" ");
+    /**
+     * Creates array of token from given string.
+     * @param code - code in single string
+     * @return array of String tokens.
+     */
+    public static String[] getToken(String code) {
+        return code.split(" ");
     }
 
-    public static void translate(String[] lineVector) {
-        if (lineVector.length > 0) {
-            Integer i = 0;
-            while (i < lineVector.length) {
-                i = checkMethod(lineVector, i);
+    /**
+     *Transforms code into required symbol representation.
+     * @param tokens - Array of String Token.
+     */
+    public static void translate(String[] tokens) {
+        if (tokens.length > 0) {
+            int i = 0;
+            while (i < tokens.length) {
+                i = checkMethod(tokens, i);
                 System.out.println("\n");
             }
         }
     }
 
+    /**
+     * Check whether given token is Method and converts it into expected symbols.
+     * @param tokens - Array of token.
+     * @param counter - array counter.
+     * @return Int updated position of array counter.
+     */
+    public static int checkMethod(String[] tokens, int counter) {
 
-    public static Integer checkMethod(String[] lineToken, Integer i) {
-
-        if (!lineToken[i].equals("if") && !lineToken[i].equals("while") && !lineToken[i].equals("}")) {
-            ++i;
-            if (lineToken[i].equals("(")) {
-                ++i;
-                if (lineToken[i].equals(")")) {
-                    ++i;
-                    if (lineToken[i].equals("{")) {
-                        ++i;
+        if (!tokens[counter].equals("if") && !tokens[counter].equals("while") && !tokens[counter].equals("}")) {
+            ++counter;
+            if (tokens[counter].equals("(")) {
+                ++counter;
+                if (tokens[counter].equals(")")) {
+                    ++counter;
+                    if (tokens[counter].equals("{")) {
+                        ++counter;
                         System.out.print("[ ");
-                        i = checkMethod(lineToken, i);
-                        i = checkIf(lineToken, i);
-                        i = checkWhile(lineToken, i);
-                        if (lineToken[i].equals("}")) {
+                        counter = checkInstruction(tokens, counter);
+                        counter = checkIf(tokens, counter);
+                        counter = checkWhile(tokens, counter);
+                        if (!tokens[counter].equals("}")) {
+                            while (!tokens[counter].equals("}")){
+                                counter = checkInstruction(tokens, counter);
+                                counter = checkIf(tokens, counter);
+                                counter = checkWhile(tokens, counter);
+                            }
                             System.out.print("] ");
-                            ++i;
+                            ++counter;
+                        } else{
+                            System.out.print("] ");
+                            ++counter;
                         }
+                    } else {
+                        throw new RuntimeException("Invalid Syntax");
                     }
+                } else {
+                    throw new RuntimeException("Invalid Syntax");
                 }
             }
             else {
-                System.out.print("- ");
+                throw new RuntimeException("Invalid Syntax");
             }
         }
-        return i;
+        return counter;
     }
 
-    public static Integer checkIf(String[] lineToken, Integer i) {
+    /**
+     * Check whether given token is instruction and converts it into expected symbols.
+     * @param tokens String Array of Token.
+     * @param counter Array counter.
+     * @return Updated Array counter.
+     */
+    public static int checkInstruction(String[] tokens, int counter){
 
-        if (lineToken[i].equals("if")) {
-            ++i;
-            if (lineToken[i].equals("(")) {
-                ++i;
-                if (lineToken[i].equals(")")) {
-                    ++i;
-                    if (lineToken[i].equals("{")) {
-                        ++i;
+        if (!tokens[counter].equals("if") && !tokens[counter].equals("while") && !tokens[counter].equals("}")) {
+            if (tokens[counter].equals("(") || tokens[counter].equals(")")) {
+                throw new RuntimeException("Invalid Syntax");
+            }
+            ++counter;
+            System.out.print("- ");
+        }
+        return counter;
+    }
+
+    /**
+     * Checks whether given token is "if" and converts it into expected symbols.
+     * @param tokens Array of String tokens.
+     * @param counter Array Counter.
+     * @return Updated Array Counter.
+     */
+    public static int checkIf(String[] tokens, int counter) {
+
+        if (tokens[counter].equals("if")) {
+            ++counter;
+            if (tokens[counter].equals("(")) {
+                ++counter;
+                if (tokens[counter].equals(")")) {
+                    ++counter;
+                    if (tokens[counter].equals("{")) {
+                        ++counter;
                         System.out.print("< ");
-                        i = checkMethod(lineToken, i);
-                        i = checkIf(lineToken, i);
-                        i = checkWhile(lineToken, i);
-                        if (lineToken[i].equals("}")) {
+                        counter = checkInstruction(tokens, counter);
+                        counter = checkIf(tokens, counter);
+                        counter = checkWhile(tokens, counter);
+                        if (tokens[counter].equals("}")) {
                             System.out.print("> ");
-                            ++i;
+                            ++counter;
                         }
                     }
                 }
             }
         }
-        return i;
+        return counter;
     }
 
-    public static Integer checkWhile(String[] lineToken, Integer i) {
+    /**
+     * Check whether given token is "While" and converts it into expected symbols.
+     * @param tokens Array of String tokens.
+     * @param counter Array Counter.
+     * @return Updated Array Counter.
+     */
+    public static int checkWhile(String[] tokens, int counter) {
 
-        if (lineToken[i].equals("while")) {
-            ++i;
-            if (lineToken[i].equals("(")) {
-                ++i;
-                if (lineToken[i].equals(")")) {
-                    ++i;
-                    if (lineToken[i].equals("{")) {
-                        ++i;
+        if (tokens[counter].equals("while")) {
+            ++counter;
+            if (tokens[counter].equals("(")) {
+                ++counter;
+                if (tokens[counter].equals(")")) {
+                    ++counter;
+                    if (tokens[counter].equals("{")) {
+                        ++counter;
                         System.out.print("( ");
-                        i = checkWhile(lineToken, i);
-                        i = checkIf(lineToken, i);
-                        i = checkMethod(lineToken, i);
-                        if (lineToken[i].equals("}")) {
+                        counter = checkWhile(tokens, counter);
+                        counter = checkIf(tokens, counter);
+                        counter = checkInstruction(tokens, counter);
+                        if (tokens[counter].equals("}")) {
                             System.out.print(") ");
-                            ++i;
+                            ++counter;
                         }
                     }
                 }
             }
         }
-        return i;
+        return counter;
     }
 }
